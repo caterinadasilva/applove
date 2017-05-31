97538009ce2caae3007c7f4ebd4b3b3dd166f52d
Syrimande <caterinadasilva@gmail.com>
2017-05-31 16:00:43 -0400

    Modal terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,16 @@
-/*
- * Archivo principal de funcionalidad de JS
- */
+(function modal(){
+	var boxes = Array.from(document.getElementsByClassName("work-services"));
+	var modal = document.getElementById("work-services-modal");
+	var modalBody = document.getElementsByClassName('modal-body')[0];
+	var close = document.getElementById("close");
+	boxes.forEach(function(box){
+		box.addEventListener("click", function(){
+			event.preventDefault();
+			modalBody.innerHTML = box.innerHTML;
+			modal.classList.remove("hidden");
+			close.addEventListener("click",function(){
+				modal.classList.add("hidden");
+			});
+		});		
+	});
+})();
